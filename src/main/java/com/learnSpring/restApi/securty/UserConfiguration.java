d2f1e06c48dbcebd319d73d3435e538d7ad41008
yslaoui <yslaouiyassine@gmail.com>
2024-08-07 12:44:46 +0100

    Storing user authentication and authorization in database

--- src/main/java/com/learnSpring/restApi/securty/UserConfiguration.java
+++ src/main/java/com/learnSpring/restApi/securty/UserConfiguration.java
@@ -8,34 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
 import java.net.http.HttpRequest;
 
 @Configuration
 public class UserConfiguration {
     @Bean
-    public InMemoryUserDetailsManager getUserManager() {
+    public UserDetailsManager getUserManager(DataSource dataSource) {
 
-        UserDetails john = User.builder()
-                                .username("john")
-                                .password("{noop}test123")
-                                .roles("EMPLOYEE")
-                                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
